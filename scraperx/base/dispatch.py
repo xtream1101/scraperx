b447b570f0f2e3a5883063b0a6a05c54a0b1c977
Eddy Hintze <eddy@hintze.co>
2019-04-25 16:02:30 -0400

    Added support to yield tasks in the dispatcher

--- scraperx/base/dispatch.py
+++ scraperx/base/dispatch.py
@@ -1,6 +1,10 @@
+import sys
 import math
+import types
+import queue
 import inspect
 import logging
+import threading
 from abc import ABC, abstractmethod
 
 from .. import config
@@ -17,28 +21,37 @@ class BaseDispatch(ABC):
         self._scraper = inspect.getmodule(self)
         self.download_cls = download_cls
         self.extract_cls = extract_cls
+
+        try:
+            # This try/except allows the user to call super() before or after
+            # self.num_tasks is set and this will not override it
+            self.num_tasks
+        except AttributeError:
+            self.num_tasks = None
+
         if not tasks:
-            tasks = self.create_tasks()
+            tasks = self.submit_tasks()
 
-        # Make sure tasks is a list and not just a single task
-        if not isinstance(tasks, (list, tuple)):
-            tasks = [tasks]
-        self.tasks = tasks
+        if isinstance(tasks, types.GeneratorType):
+            self.tasks = tasks
+        else:
+            if not isinstance(tasks, (list, tuple)):
+                # Make sure tasks is a list and not just a single task
+                tasks = [tasks]
 
-        if config['DISPATCH_LIMIT']:
-            self.tasks = self.tasks[:config['DISPATCH_LIMIT']]
-
-        self.qps = self._get_qps()
-        self.submit_task = rate_limited(num_calls=self.qps)(self.submit_task)
+            self.num_tasks = len(tasks)
+            # Create generator
+            self.tasks = iter(tasks)
 
     @abstractmethod
-    def create_tasks(self):
-        """Generate a list of tasks to be dispatched
+    def submit_tasks(self):
+        """Return/yield tasks to be dispatched
 
         Scraper must implement this
 
         Returns:
-            list -- list of tasks (dicts)
+            list|dict|yield -- list of tasks (dicts) or a dict of a single task
+                               or a generator which yields dict's
 
         Decorators:
             abstractmethod
@@ -54,24 +67,63 @@ class BaseDispatch(ABC):
         rate_limit_type = config['DISPATCH_RATELIMIT_TYPE']
         rate_limit_value = config['DISPATCH_RATELIMIT_VALUE']
         if rate_limit_type == 'period':
-            return rate_limit_from_period(len(self.tasks), rate_limit_value)
+            return rate_limit_from_period(self.num_tasks, rate_limit_value)
         else:
             return rate_limit_value
 
-    def dispatch(self):
+    def dispatch(self, *args, **kwargs):
         """Spin up the threads to send the tasks in
         """
-        logger.info(f"Dispatch {len(self.tasks)}",
+        if self.num_tasks is None:
+            # a generator must have been passed in
+            logger.critical(("Dispatch self.num_tasks must be set if using"
+                             " a generator for tasks"),
+                            extra={'scraper_name': config['SCRAPER_NAME'],
+                                   'task': None,  # No task yet
+                                   })
+            sys.exit(1)
+
+        if config['DISPATCH_LIMIT']:
+            self.num_tasks = min(self.num_tasks, config['DISPATCH_LIMIT'])
+
+        qps = self._get_qps()
+        logger.info(f"Dispatch {self.num_tasks}",
                     extra={'scraper_name': config['SCRAPER_NAME'],
                            'task': None,  # No task yet
-                           'qps': self.qps,
+                           'qps': qps,
                            'dispatch_service': config['DISPATCH_SERVICE_NAME'],
-                           'num_tasks': len(self.tasks)})
+                           'num_tasks': self.num_tasks})
         # Have 3 times the numbers of threads so a task will not bottleneck
-        num_threads = math.ceil(self.qps * 3)
-        threads(num_threads,
-                self.tasks,
-                self.submit_task)
+        num_threads = math.ceil(qps * 3)
+        q = queue.Queue()
+
+        def _thread_run():
+            while True:
+                item = q.get()
+                try:
+                    self.submit_task(item, *args, **kwargs)
+                except Exception:
+                    logger.critical("Dispatch failed",
+                                    extra={'task': item,
+                                           'scraper_name': config['SCRAPER_NAME']},
+                                    exc_info=True)
+                q.task_done()
+
+        for i in range(num_threads):
+            t = threading.Thread(target=_thread_run)
+            t.daemon = True
+            t.start()
+
+        @rate_limited(num_calls=qps)
+        def rate_limit_tasks():
+            q.put(next(self.tasks))
+
+        # Fill the Queue with the data to process
+        for _ in range(self.num_tasks):
+            rate_limit_tasks()
+
+        # Process the data
+        q.join()
 
     def submit_task(self, task):
         """Send a single task off to be downloaded
