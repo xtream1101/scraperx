eacd85eeb5edc314332da8e86a7315097ed64448
Eddy Hintze <eddy@hintze.co>
2019-06-18 14:09:55 -0400

    version bump to 0.0.4rc10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setup(name='scraperx',
       packages=find_packages(),
-      version='0.0.4rc9',
+      version='0.0.4rc10',
       python_requires='>=3.6.0',
       license='MIT',
       description='ScraperX SDK',
