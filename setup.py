205cbf5ce33cb655f1702dec4984456828a6a02a
Eddy Hintze <eddy@hintze.co>
2019-07-11 15:48:34 -0400

    Added missing requirement

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 
 setup(name='scraperx',
       packages=find_packages(),
-      version='0.2.0',
+      version='0.2.1',
       python_requires='>=3.6.0',
       license='MIT',
       description='ScraperX SDK',
