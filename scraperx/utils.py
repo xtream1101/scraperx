f0eeba19f1f4699b86dc02e0ffc917ea1a96246f
Eddy Hintze <eddy@hintze.co>
2023-03-28 15:58:16 -0400

    Renable encoding checking

--- scraperx/utils.py
+++ scraperx/utils.py
@@ -16,8 +16,7 @@ def get_encoding(file_bytes):
     Returns:
         str: Name of the encoding used
     """
-    return 'utf-8'
-    # return charset_normalizer.detect(file_bytes)['encoding']
+    return charset_normalizer.detect(file_bytes)['encoding']
 
 
 def read_file_contents(file_name, transport_params={}):
